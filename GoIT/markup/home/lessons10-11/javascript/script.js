8d9e02c5b6b3745446092eceabb66ae77a5aff19
Dmitriy <didyk.d.s@gmail.com>
2015-12-03 17:25:26 +0200

    upd 10-11

--- GoIT/markup/home/lessons10-11/javascript/script.js
+++ GoIT/markup/home/lessons10-11/javascript/script.js
@@ -1,25 +1,35 @@
-function myVeriablePow(){
+var myVeriableNumber = +prompt('Введите число, которое будем возводить в степень');
+var myVeriableExponent = +prompt('Введите степень, в которую будем возводить');
 
-var myVeriableNumber;
-var myVeriableExponent;
-var myVeriableResult;
 
-myVeriableNumber = prompt('Enter a number');
-myVeriableExponent = prompt('Enter a exponent');
+function pow(base, exponent) {
+  var myVeriableResult = 1;
 
-myVeriableResult = Math.pow(myVeriableNumber, myVeriableExponent);
-// alert (myVeriableResult);
-console.log('Result = ', myVeriableResult);
+  for (var i = 0; i < exponent; i++) {
+    myVeriableResult = myVeriableResult * base;
+  }
+
+  return myVeriableResult;
 }
 
-myVeriablePow();
+var myVeriablePowResult = pow(myVeriableNumber, myVeriableExponent);
+
+console.log(myVeriablePowResult);
 // ======================== 1 simple ends===================
 
 var myVeriableArr = [];
 
 for(var i = 0; i <= 4; i++){
-
 myVeriableArr[i] = prompt('Enter ' + [i+1] + ' favorite names');
+  if (myVeriableArr[i] === null){
+        alert('Вы отменили!!'); 
+        myVeriableArr = []
+        break;
+  } else if(myVeriableArr[i] === ""){
+        alert('Введите имя!!'); 
+        myVeriableArr = []
+        break;
+  }
 }
 
 var myVeriableUsername = prompt('Enter a username');
@@ -30,12 +40,14 @@ console.log('Arr = ', myVeriableArr);
 console.log('Username = ', myVeriableUsername);
 
 for (var i = 0; i < myVeriableArr.length; i++) {
-  if (myVeriableUsername == myVeriableArr[i]) {
+  if (myVeriableUsername === myVeriableArr[i]) {
   	myVeriableError = 1;
+  	break;
   }
 }
-if (myVeriableError == 1) {
+if (myVeriableError === 1) {
   alert(myVeriableUsername + ' you have successfully logged!');	
 } else{
-  	alert('Error!');
+  	alert('Ошибка, вы ввели не правильное имя или пропустили строку при вводе');
 }
+// ======================== 2 simple ends===================
