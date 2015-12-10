e7e5d2f52b64bb69bffb6addabbb0f16bf62f41f
Dmitriy <didyk.d.s@gmail.com>
2015-12-10 22:28:46 +0200

    add homework 12-13

---
+++ GoIT/markup/home/hard12-13/javascript/script.js
@@ -0,0 +1,82 @@
+var domElements = {
+
+   domElementsCreate: function(params) {
+      var element = document.createElement(params.tagName);
+
+      // element.classList.add(params);
+      if (params.inputType){
+      element.setAttribute('type', params.inputType);
+      }
+
+      if(params.className){
+        element.className = params.className;
+      }
+
+      if(params.content){
+        element.innerHTML = params.content;
+      }
+
+      if(params.parentElement){
+        params.parentElement.appendChild(element);
+      }
+
+      return element;
+   },
+
+    generateQuestions: function(questionsAmount, answersAmount){    
+    
+      console.log (this);
+      for (var i = 0; i < questionsAmount; i++) {
+
+        this.domElementsCreate({
+        tagName: 'h2',
+        className: 'main__questions',
+        content: (i + 1)+ '. ' + 'Вопрос №' + (i + 1),
+        parentElement: mainDivHTML
+      });
+
+      for (var j = 0; j < answersAmount; j++) {
+        var checkbox = this.domElementsCreate({
+          tagName: 'input',
+          className: 'main__checkbox',
+          inputType: 'checkbox',
+          parentElement: mainDivHTML
+        });
+
+        var labelHTML = this.domElementsCreate({
+          tagName: 'label',
+          className: 'main__answers',
+          content: 'Вариант ответа №' + (j + 1),
+          parentElement: mainDivHTML
+        });
+
+
+      }
+      }
+  }
+}
+
+var bodyHTML = document.querySelector('body');
+domElements.domElementsCreate({
+  tagName: 'div',
+  className: 'main',
+  parentElement: bodyHTML
+});
+
+var mainDivHTML = document.querySelector('.main');
+domElements.domElementsCreate({
+  tagName: 'H1',
+  className: 'title',
+  content: 'Тест по программированию',
+  parentElement: mainDivHTML
+});
+
+domElements.generateQuestions(3, 3);
+
+domElements.domElementsCreate({
+  tagName: 'input',
+  inputType: 'submit',
+  className: 'main__submit',
+  content: 'Проверить мои результаты',
+  parentElement: mainDivHTML
+});
