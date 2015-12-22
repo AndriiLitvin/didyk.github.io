a2cee895b6c262746d0429404989664b5d313a42
Dmitriy <didyk.d.s@gmail.com>
2015-12-22 16:37:24 +0200

    upd

--- GoIT/markup/home/lessons14-15/javascript/script.js
+++ GoIT/markup/home/lessons14-15/javascript/script.js
@@ -1,8 +1,10 @@
+var date = new Date();
 var timer;
 var counter = 0;
-var msecs = '00';
+var msecs = '000';
 var secs = '00';
 var minutes = '00';
+var hours = '00';
 
 startB.addEventListener('click', startTimer);
 stopB.addEventListener('click', resetTimer);
@@ -16,26 +18,28 @@ function startTimer()
    timer = setInterval(
    function () {
    msecs++;
-   if (msecs < 10){
+   // if (msecs < 10){
+   //     msecs = '00' + msecs;
+   //     };
+   if (msecs < 100){
        msecs = '0' + msecs;
        };
-   if (msecs === 100) {
+   if (msecs == 200) {
        secs++;
        if (secs < 10){
        secs = '0' + secs;
        };
-       msecs = '00';
+       msecs = '000';
        };
-   if (secs === 60) {
+   if (secs == 60) {
        minutes++;
        if (minutes < 10){
        minutes = '0' + minutes;
        };
        secs = '00';
        };
-       document.getElementById('timer').innerHTML = minutes + ':' + secs + ':' + msecs;
-     },
-     10);
+       document.getElementById('timer').innerHTML = hours + ':' + minutes + ':' + secs + ':' + msecs;
+     }, 5);
        document.getElementById('startB').innerHTML = 'Пауза';
      } else {
        window.clearInterval(timer);
@@ -49,6 +53,7 @@ function resetTimer()
      msecs = 0;
      secs = 0;
      minutes = 0;
-     document.getElementById('timer').innerHTML = '00:00:00';
+     var hours = 0;
+     document.getElementById('timer').innerHTML = '00:00:00:000';
      document.getElementById('startB').innerHTML = 'Старт';
      }
